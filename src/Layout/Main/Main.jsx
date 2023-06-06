b9e279e05051a06941e77a0db62c58f385fa166d
wahidgoni17 <imwahidgoni17@gmail.com>
2023-06-06 23:21:00 +0600

    footer added

--- src/Layout/Main/Main.jsx
+++ src/Layout/Main/Main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../../Pages/Shared/Header&Footer/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../../Pages/Shared/Header&Footer/Footer';
 
 const Main = () => {
     return (
         <>
             <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </>
     );
 };
--- src/Pages/Shared/Header&Footer/Header.jsx
+++ src/Pages/Shared/Header&Footer/Header.jsx
@@ -1,82 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaBars, FaUserAlt } from "react-icons/fa";
 
 const Header = () => {
   return (
     <>
-      <div className="navbar py-4 px-6 bg-slate-100">
+      <div className="navbar py-4 px-6 bg-[#eaffff]">
         <div className="navbar-start">
           <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost btn-circle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h7"
-                />
-              </svg>
+            <label tabIndex={0} className="btn btn-ghost btn-circle text-xl">
+              <FaBars />
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-5 p-3 shadow rounded-box w-56 h-40"
+              className="menu text-xl dropdown-content mt-5 p-3 shadow rounded-box w-56"
             >
               <li>
-                <a>Homepage</a>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <a>Portfolio</a>
+                <Link>Instructors</Link>
               </li>
               <li>
-                <a>About</a>
+                <Link>Classes</Link>
+              </li>
+              <li>
+                <Link to="dashboard">Dashboard</Link>
               </li>
             </ul>
           </div>
         </div>
         <div className="navbar-center">
-          <a className="btn btn-ghost normal-case  text-4xl font-bold" >Fluentia</a>
+          <Link
+            to="/"
+            className="btn btn-ghost normal-case  text-4xl font-bold"
+          >
+            Fluentia
+          </Link>
         </div>
         <div className="navbar-end">
-          <button className="btn btn-ghost btn-circle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </button>
-          <button className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
-              <span className="badge badge-xs badge-primary indicator-item"></span>
-            </div>
-          </button>
+          <div className="text-2xl mr-5">
+            <FaUserAlt />
+          </div>
+          <Link to="/login">
+            <button className="btn btn-primary btn-outline border-0 border-y-2">
+              LogIn
+            </button>
+          </Link>
         </div>
       </div>
     </>
